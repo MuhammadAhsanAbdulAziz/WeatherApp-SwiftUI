764084f0b310305c8b95e378f2fb076da7fc5f9f
Muhammad Ahsan bin Abdul Aziz <maahsan36@gmail.com>
2023-11-16 17:08:36 +0500

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Macbook Pro on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
